67895b3ebc6f00f6fce3eb36058428081fca0bb1
Shohei Aoki <shoaok@gmail.com>
2018-05-15 14:49:34 +0900

    test

--- javascripts/blockly/generators/python/gcode.js
+++ javascripts/blockly/generators/python/gcode.js
@@ -28,6 +28,7 @@ Blockly.Python['gcode_send_string'] = function(block) {
   var value_gcode = Blockly.Python.valueToCode(block, 'gcode', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
   var code = 'arduino.write('+value_gcode+'\\n)\n';
+  var moge = 1;
   return code;
 };
 
